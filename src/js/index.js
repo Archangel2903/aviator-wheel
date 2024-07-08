a16046415d2763f46aa96fa630d66c3e33414652
Archangel2903 <archangel2903@gmail.com>
2024-07-09 00:20:45 +0300

    done

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import '../scss/main.scss';
 import $ from 'jquery';
 import 'bootstrap';
 import 'popper.js';
+import select2 from 'select2/dist/js/select2.full.min';
 
 $(window).on('load', function () {
     let b = $('body');
@@ -14,6 +15,76 @@ $(window).on('load', function () {
 });
 
 $(function () {
+    // wheel&modals
+    const wheel = $('.section-wheel__wheel-circle img');
+    const btnSpin = $('#button');
+    const first_modal = $('#firstTry');
+    const next_try = $('#nexTry');
+    const second_modal = $('#secondTry');
+    const to_registration = $('#finalStep');
+    const registration_modal = $('#registration');
+    const counter = $('.section-wheel__counter');
+
+    function counterData(el) {
+        let currentVal = $(el).attr('data-counter');
+
+        $(el).attr('data-counter', currentVal - 1);
+    }
+
+    btnSpin.on('click', function () {
+        if (!wheel.hasClass('step-2')) {
+            wheel.addClass('step-1');
+            counterData(counter);
+
+            setTimeout(function () {
+                first_modal.modal({
+                    backdrop: 'static',
+                    show: true,
+                });
+            }, 4000);
+        }
+        else {
+            registrationModal.modal('show');
+        }
+    });
+    next_try.on('click', function () {
+        first_modal.modal('hide');
+        wheel.toggleClass('step-1 step-2');
+        counterData(counter);
+
+        setTimeout(function () {
+            second_modal.modal({
+                backdrop: 'static',
+                show: true,
+            });
+        }, 4000);
+    });
+    to_registration.on('click', function () {
+        second_modal.modal('hide');
+        registration_modal.modal({
+            backdrop: 'static',
+            show: true,
+        });
+    });
+
+    // Select2
+    let selectStyled = $('.select2');
+
+    for (let i = 0; i <= selectStyled.length; i++) {
+        if (selectStyled.hasClass('js-example-placeholder-single')) {
+            selectStyled.select2({
+                width: '100%',
+                placeholder: "Select a state",
+                minimumResultsForSearch: Infinity,
+            });
+        } else {
+            selectStyled.select2({
+                width: '100%',
+                minimumResultsForSearch: Infinity,
+            });
+        }
+    }
+
     // Password switch
     const passBtn = document.querySelector('.form-button-pass');
     if (passBtn) {
